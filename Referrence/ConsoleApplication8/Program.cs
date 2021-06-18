072e7919105ec8ec50c19b1cf575900c1cb098fb
Mustafizur Rahman <mrahaman59@yahoo.com>
2021-06-18 20:10:35 +0600

    Referrence

---
+++ Referrence/ConsoleApplication8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+     {
+          int x = 4, y = 5;
+
+          Swap(ref x, ref y);
+
+          Console.WriteLine(x+ "\t" +y);
+     }​​​​​​
+
+        static void Swap( ref int a, ref int b)
+        {​​​​​​
+           int c;
+
+           c = a;
+           a = b;
+           b = c;
+        }
+    }
+}
