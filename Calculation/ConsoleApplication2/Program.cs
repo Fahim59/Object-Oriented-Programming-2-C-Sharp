fe1c73ed9fe2d08afb3c2cdef1c7c6a64a1f059d
Mustafizur Rahman <mrahaman59@yahoo.com>
2021-06-18 19:59:00 +0600

    If-Else Problem

---
+++ Calculation/ConsoleApplication2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1,num2,result;
+            string opt;
+
+            num1 = Convert.ToDouble(Console.ReadLine());
+            num2 = Convert.ToDouble(Console.ReadLine());
+            opt = Console.ReadLine();
+
+            if(opt == "+")
+            {
+                result = num1 + num2;
+                Console.WriteLine(result);
+            }
+            else if(opt == "-")
+            {
+                result = num1 - num2;
+                Console.WriteLine(result);
+            }
+            else if(opt == "*")
+            {
+                result = num1 * num2;
+                Console.WriteLine(result);
+            }
+            else if(opt == "/")
+            {
+                result = num1 / num2;
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+        }
+    }
+}
